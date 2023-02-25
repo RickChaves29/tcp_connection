32feb32f8e75caeaec2f56ec610e1c9692575b0f
Henrique Chaves <canaldomonza@gmail.com>
2023-02-25 18:05:03 -0300

    test: create test to function remount payload when recive id

--- server/domain/usecases/usecases_test.go
+++ server/domain/usecases/usecases_test.go
@@ -6,6 +6,14 @@ import (
 	"github.com/RickChaves29/tcp_service/server/domain/usecases"
 )
 
+func TestShoudReturnRemountPayloadWithCorrectID(t *testing.T) {
+	data := []byte("         ab33bc35-a29a-49df-b1d6-75e1366fee6e           \x00\x00\x00\x00")
+	resultWant := []byte("ab33bc35-a29a-49df-b1d6-75e1366fee6e")
+	resultHave := usecases.RemountPayload(data)
+	if string(resultHave) != string(resultWant) {
+		t.Errorf("result have %v but want %v", resultHave, resultWant)
+	}
+}
 func TestShoudReturnRemountPayloadWithCorrectAction(t *testing.T) {
 	data := []byte("         LIST           \x00\x00\x00\x00")
 	resultWant := []byte("LIST")
