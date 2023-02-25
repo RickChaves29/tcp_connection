e5c7f3bb5d53c19a13cfb341c52852a93fd34a0d
Henrique Chaves <canaldomonza@gmail.com>
2023-02-25 20:31:02 -0300

    feat: function to set when client is connected

--- server/main.go
+++ server/main.go
@@ -1,59 +1,38 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net"
-	"regexp"
+	"os"
 
+	"github.com/RickChaves29/tcp_service/server/domain/usecases"
 	"github.com/google/uuid"
 )
 
-type Client struct {
-	ID      string
-	Host    string
-	Payload string
-}
-
 func main() {
-	listen, err := net.Listen("tcp", ":4040")
+	PORT := fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))
+	repository := []usecases.ClientEntity{}
+	uc := usecases.NewUsecase(repository)
+
+	l, err := net.Listen("tcp", PORT)
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalf("LOG - [ERROR]: %v\n", err.Error())
 	}
-	defer listen.Close()
 	for {
-		conn, err := listen.Accept()
+		conn, err := l.Accept()
 		if err != nil {
-			log.Fatalln(err.Error())
-			continue
+			log.Fatalf("LOG - [ERROR]: %v\n", err.Error())
 		}
-		go handlerConnection(conn)
+		id := uuid.New().String()
+		clientHost := conn.RemoteAddr().String()
+		clientID, err := uc.AddNewClient(id, clientHost)
+		log.Printf("LOG - [CONNECTED] host: %s client_id: %s\n", clientHost, clientID)
+		if err != nil {
+			log.Printf("LOG - [ERROR]: %v\n", err.Error())
+		}
+		serverMessage := fmt.Sprintf("Welcome your id is %s\n", clientID)
+		conn.Write([]byte(serverMessage))
 	}
 
-}
-
-func handlerConnection(conn net.Conn) {
-	defer conn.Close()
-	payload := make([]byte, 1024)
-	rx, err := regexp.Compile(`[\n\s]`)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	id := uuid.New().String()
-	fmt.Printf("new connection from host: %s id: %s\n", conn.RemoteAddr(), id)
-	for {
-		_, err := conn.Read(payload)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		data := rx.ReplaceAll(bytes.Trim(payload, "\x00"), []byte(""))
-		if string(data) == "LIST" {
-			fmt.Println("list data: ", string(data))
-		} else if string(data) == "RELAY" {
-			fmt.Println("replay data: ", string(data))
-		} else {
-			fmt.Println("any data: ", string(data))
-		}
-	}
 }
