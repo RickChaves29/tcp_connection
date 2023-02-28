73f02607c84da4ee4efddc6e4209229a0ba4454a
Henrique Chaves <canaldomonza@gmail.com>
2023-02-27 21:18:07 -0300

    feat: add function to send bug from all hosts

--- server/presenter/action_presenter.go
+++ server/presenter/action_presenter.go
@@ -23,8 +23,11 @@ func (ap *ActionPresenter) SetAction() {
 	var (
 		id, action string
 	)
+	var connections = make(map[string]net.Conn)
+
 	idPayload := make([]byte, 1024)
 	actionPayload := make([]byte, 1024)
+	bodyPayload := make([]byte, 1024)
 	_, err := ap.conn.Read(idPayload)
 	if err != nil {
 		log.Printf("LOG - [ERROR]: %v", err.Error())
@@ -33,8 +36,15 @@ func (ap *ActionPresenter) SetAction() {
 	if err != nil {
 		log.Printf("LOG - [ERROR]: %v", err.Error())
 	}
+	_, err = ap.conn.Read(bodyPayload)
+	if err != nil {
+		log.Printf("LOG - [ERROR]: %v", err.Error())
+	}
 	id = string(usecases.RemountPayload(idPayload))
 	action = string(usecases.RemountPayload(actionPayload))
+	body := string(usecases.RemountPayload(bodyPayload))
+	connections[id] = ap.conn
+
 	switch action {
 	case "LIST":
 		clients, err := ap.usecase.ListAllClientsID(id, action)
@@ -43,6 +53,17 @@ func (ap *ActionPresenter) SetAction() {
 		}
 		for _, client := range clients {
 			_, err = ap.conn.Write([]byte("client -> " + client + "\n"))
+			if err != nil {
+				log.Printf("LOG - [ERROR]: %v", err.Error())
+			}
+		}
+	case "RELAY":
+		_, err := ap.usecase.ListAllClientsID(id, action)
+		if err != nil {
+			log.Printf("LOG - [ERROR]: %v", err.Error())
+		}
+		for _, conn := range connections {
+			_, err := conn.Write([]byte(body))
 			if err != nil {
 				log.Printf("LOG - [ERROR]: %v", err.Error())
 			}
