899f0d1022ecf2c75f27774f35480d843ecf24ad
Henrique Chaves <canaldomonza@gmail.com>
2023-03-01 22:38:46 -0300

    feat: open client connection

--- client/main.go
+++ client/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -15,45 +12,4 @@ func main() {
 		log.Fatalf("LOG - [ERROR]: %v", err.Error())
 	}
 	defer conn.Close()
-
-	readConnection := bufio.NewReader(conn)
-
-	response, err := readConnection.ReadString('\n')
-	if err != nil {
-		if err != nil {
-			log.Printf("LOG - [ERROR]: %v", err.Error())
-		}
-	}
-	log.Println(strings.TrimSpace(response))
-	var id string
-	var action string
-	var body string
-	_, err = fmt.Scan(&id)
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-
-	_, err = fmt.Scan(&action)
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-
-	_, err = fmt.Scan(&body)
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-
-	_, err = conn.Write([]byte(strings.TrimSpace(id)))
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-	_, err = conn.Write([]byte(strings.TrimSpace(action)))
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-	_, err = conn.Write([]byte(strings.TrimSpace(body)))
-	if err != nil {
-		log.Printf("LOG - [ERROR]: %v", err.Error())
-	}
-
 }
