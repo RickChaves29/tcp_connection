111af3202d885df1c06e33d390bbbabf7211a18d
Henrique Chaves <canaldomonza@gmail.com>
2023-03-03 19:33:00 -0300

    recfactor: remount main file

--- server/main.go
+++ server/main.go
@@ -1,45 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
-	"os"
 
 	"github.com/RickChaves29/tcp_service/server/domain/usecases"
 	"github.com/RickChaves29/tcp_service/server/presenter"
-	"github.com/google/uuid"
 )
 
-var connections = make(map[string]net.Conn)
-
 func main() {
-	PORT := fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))
-	repository := []usecases.ClientEntity{}
-	uc := usecases.NewUsecase(repository)
-
-	l, err := net.Listen("tcp", PORT)
+	uc := usecases.NewUsecase([]usecases.ClientEntity{})
+	server, err := presenter.NewPresenter(uc)
 	if err != nil {
-		log.Fatalf("LOG - [ERROR]: %v\n", err.Error())
+		log.Printf("LOG - [test-start-error]: %v", err)
+		return
 	}
-	defer l.Close()
 	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatalf("LOG - [ERROR]: %v\n", err.Error())
-		}
-		id := uuid.New().String()
-		connections[id] = conn
-		clientHost := conn.RemoteAddr().String()
-		clientID, err := uc.AddNewClient(id, clientHost)
-		log.Printf("LOG - [CONNECTED] host: %s client_id: %s\n", clientHost, clientID)
-		if err != nil {
-			log.Printf("LOG - [ERROR]: %v\n", err.Error())
-		}
-		serverMessage := fmt.Sprintf("Welcome your id is %s\n", clientID)
-		conn.Write([]byte(serverMessage))
-		ap := presenter.NewActionPresenter(conn, uc)
-		go ap.SetAction(connections)
+		server.Start()
 	}
-
 }
