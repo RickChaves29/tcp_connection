18931694cb89baf9b8dfc35f9dc49d923259c761
Henrique Chaves <canaldomonza@gmail.com>
2023-03-06 21:23:36 -0300

    feat: add io reader and white with connection from server

--- client/main.go
+++ client/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":"+os.Getenv("SERVER_PORT"))
+	conn, err := net.Dial("tcp", ":3000")
 	if err != nil {
-		log.Fatalf("LOG - [ERROR]: %v", err.Error())
+		log.Fatalf("LOG - [error]: %v", err.Error())
 	}
 	defer conn.Close()
+	c := make(chan string)
+	go func(c chan string) {
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil {
+			log.Printf("LOG - [io-error]: %v\n", err.Error())
+		}
+
+		c <- "receive payload\n"
+	}(c)
+	go func(c chan string) {
+		_, err := io.Copy(conn, os.Stdin)
+		if err != nil {
+			log.Printf("LOG - [io-error]: %v\n", err.Error())
+		}
+
+		c <- "send payload\n"
+	}(c)
+
+	fmt.Printf(<-c)
+
 }
